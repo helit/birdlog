6b1a46256dfa88b6ac92adf1b0fc834bb1bcd72d
Henrik Littke <henlit@Henriks-MacBook-Pro.local>
2026-03-09 15:05:09 +0100

    client apollo auth link

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import App from "./App.js";
 import "./index.css";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
